bef7b2fd089f2800fa32746704e31f263649ec04
Kousuke Takaki <u7693@protonmail.com>
2019-09-28 07:14:22 +0000

    [update] template.cppに色々追加
    - void Main() をインライン関数にする
    - vectorを出力するためのテンプレートを追加

--- library/misc/template.cpp
+++ library/misc/template.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
+#include <boost/range/adaptor/indexed.hpp>
 using namespace std;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define all(x) (x).begin(), (x).end()
 
-void Main() {
+template <typename T>
+std::ostream &operator<<(std::ostream &ostr, const std::vector<T> &v) {
+  for (const auto &i : v | boost::adaptors::indexed())
+    ostr << (i.index() > 0 ? " " : "") << i.value();
+  return ostr;
+}
+
+inline void Main() {
   // code
 }
 
 int main() {
-  cin.tie(0);
-  ios::sync_with_stdio(false);
-  cout << fixed << setprecision(20);
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(20);
   Main();
   return 0;
 }
