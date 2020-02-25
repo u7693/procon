c6a35e1022b78ecfc0b4c2b0c4a51e7fc306080a
Kousuke Takaki <u7693@protonmail.com>
2020-02-25 17:23:32 +0900

    ファイル名を変更

--- ant/src/easy/mod.rs
+++ ant/src/easy/mod.rs
@@ -1,8 +1,8 @@
-pub mod best_cow_line;
-pub mod coin_problem;
-pub mod fence_repair;
-pub mod interval_scheduling;
-pub mod lake_counting;
-pub mod maze_shortest_path;
-pub mod sarumans_army;
-pub mod subset_sum_problem;
+pub mod a_1_subset_sum_problem;
+pub mod a_2_lake_counting;
+pub mod a_3_maze_shortest_path;
+pub mod b_1_coin_problem;
+pub mod b_2_interval_scheduling;
+pub mod b_3_best_cow_line;
+pub mod b_4_sarumans_army;
+pub mod b_5_fence_repair;
