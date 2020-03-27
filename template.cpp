d905e1413894078ec8213a185ef1769a4386dbeb
Kousuke Takaki <u7693@protonmail.com>
2020-03-27 20:35:23 +0900

    C++のテンプレートを追加

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define repi(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define rep(i, n) repi(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+
+inline void Main() {
+  // code
+}
+
+int main() {
+  std::cin.tie(0);
+  std::ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(20);
+  Main();
+  return 0;
+}
