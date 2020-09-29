c1831b093ad63423ed1d4c19d2e5dbd65a2aad8f
Kousuke Takaki <u7693@protonmail.com>
2020-09-29 19:52:31 +0900

    [library]: Add gcd & lcm

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,21 @@ pub fn divisor(n: u64) -> Vec<u64> {
     r
 }
 
+#[snippet]
+pub fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+#[snippet]
+#[snippet(include = "gcd")]
+pub fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -26,6 +41,22 @@ mod tests {
         let testcases = vec![(3, vec![1, 3]), (6, vec![1, 2, 3, 6]), (25, vec![1, 5, 25])];
         for testcase in testcases {
             assert_eq!(divisor(testcase.0), testcase.1);
+        }
+    }
+
+    #[test]
+    fn test_gcd() {
+        let testcases = vec![(11, 121, 11), (18, 27, 9), (21, 56, 7)];
+        for testcase in testcases {
+            assert_eq!(gcd(testcase.0, testcase.1), testcase.2);
+        }
+    }
+
+    #[test]
+    fn test_lcm() {
+        let testcases = vec![(0, 1, 0), (18, 27, 54), (49, 77, 539)];
+        for testcase in testcases {
+            assert_eq!(lcm(testcase.0, testcase.1), testcase.2);
         }
     }
 }
