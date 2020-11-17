e8ef280cacce907b8b587fa72bdc71ab8d46033b
Kousuke Takaki <u7693@protonmail.com>
2020-11-17 15:23:08 +0900

    Remove euler

--- euler/1.py
+++
@@ -1,9 +0,0 @@
-def solve(x):
-    result = 0
-    for i in range(1, x):
-        if i % 3 == 0 or i % 5 == 0:
-            result += i
-    return result
-
-assert solve(10) == 23
-print(solve(1000))
